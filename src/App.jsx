74f3195d0950b3adf88c670a96bfa834769f33f1
Brian <briantechdom@gmail.com>
2024-04-01 11:00:16 -0700

    Set up useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import './App.css'
 function App() {  
   const [coins, setCoins] = useState([])
 
-
+  useEffect(() => {
+     axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd").then(res => {
+      setCoins(res.data);
+     })
+  
+  })
 
   return (
     <>
