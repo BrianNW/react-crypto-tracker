8dea697049bb283c13f20b37c110d87b858a2483
Brian <briantechdom@gmail.com>
2024-04-01 09:02:33 -0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
 
   return (
     <>
       <div>
-        <h1>Cryptoyz Tracker API</h1>
+        <h1>Cryptoyz Tracker</h1>
       </div>
       
     </>
