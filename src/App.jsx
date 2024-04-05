45741042ecd0c2ab74ac75b8f759da808a33011d
Brian <briantechdom@gmail.com>
2024-04-05 15:36:43 -0700

    Add input for form element

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <div className = 'coin-search'>
           <h1 className="coin-text">Search a currency</h1>
           <form> 
-            
+            <input type="text" placeholder="Search" className="coin-input" />
           </form>
         </div>
       </div>
