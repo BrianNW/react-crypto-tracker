19edb9aca9d593bc15ae5fd8f8d7fd55f83b1e2a
Brian <briantechdom@gmail.com>
2024-04-25 10:49:19 -0700

    Added localization for volume ticker

--- src/Coin.jsx
+++ src/Coin.jsx
@@ -12,7 +12,7 @@ const Coin = ({ name, image, symbol, price, volume, priceChange }) => {
             </div>
             <div className="coin-data">
               <p className="coin-price"> ${price} </p>
-              <p className="coin-volume">${volume}</p>              
+              <p className="coin-volume">${volume.toLocaleString()}</p>              
               {priceChange < 0 ? (
               <p className="coin-percent red"> {priceChange.toFixed(2)} %</p> ) : (
                 <p className="coin-percent green"> {priceChange.toFixed(2)}%</p>)
